0f28364f14d84551db9a766097909c3a86d53d0f
Ranaivonampoizina Mikajy <mikamickswonder@gmail.com>
2022-12-03 19:11:21 +0100

    get ready for the first file systeme trying on android

--- domy/www/js/app.js
+++ domy/www/js/app.js
@@ -5,20 +5,6 @@ numberInput.focus();
 const log = document.getElementById('values');
 
 
-function addPercent(num) {
-  percent = (num / 100) * 3;
-  result = num + percent;
-  return result;
-}
-
-function calculate(number, days) {
-  newSolde = number;
-  for (let i = 0; i < days; i++) {
-    newSolde = addPercent(newSolde);
-  }
-  return newSolde;
-}
-
 daysInput.addEventListener('input', updateValue);
 
 function updateValue(e) {
@@ -28,25 +14,6 @@ function updateValue(e) {
   var ton_chiffre = calculate(number, days); // Ta variable de chiffre
   const response = (ton_chiffre.toFixed(2));
 
-  if (isNaN(response)) {
-    log.textContent = "0.00";
-  } else {
-    log.textContent = response;
-  }
-}
-
-numberInput.addEventListener('input', updateValue1);
-
-function updateValue1(e) {
-  var number = parseInt(e.target.value);
-  var days = parseInt(daysInput.value);
-  if (!days) {
-    var ton_chiffre = number; // Ta variable de chiffre
-  }else{
-    var ton_chiffre = calculate(number, days);
-  }
-  
-  const response = (ton_chiffre.toFixed(2));
   if (isNaN(response)) {
     log.textContent = "0.00";
   } else {
